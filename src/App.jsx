d09c072e19686a59992f781a5e49fa2bb9daabdc
Alexandre Gaia <git config --global user.email>
2024-05-25 18:18:31 -0300

    Initial Configs

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { GlobalStyle } from './styles'
+
+function App() {
+
+  return(
+    <>
+    <GlobalStyle/>
+    <div>
+      <h1>Testing</h1>
+    </div>
+    </>
+  )
+
+}
+
+export default App
